537163a49fa8382d532adfe8766b8a599a02aa94
yishaihl <yishaihl@gmail.com>
2019-07-11 18:50:49 +0300

    8th Commit

--- Automating “BuyMe” website - Project/Automating_Buy_Me_Website.py
+++ Automating “BuyMe” website - Project/Automating_Buy_Me_Website.py
@@ -1,13 +1,10 @@
 #Import Required Packages:
 from time import sleep
-from selenium.webdriver.remote.webelement import WebElement
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 import time
 #Project variable:
 driver_location = "//Users//yishaihl//Desktop//ChromeDriver"
@@ -25,28 +22,29 @@ driver.maximize_window()
 driver.get(website_url)
 driver.implicitly_wait(30)
 #Press on button "״כניסה והרשמה:
-#search_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "seperator-link")))
-#ActionChains(driver).move_to_element(search_element).perform()
-#click_item = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "seperator-link")))
-#click_item.click()
+search_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "seperator-link")))
+ActionChains(driver).move_to_element(search_element).perform()
+click_item = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "seperator-link")))
+click_item.click()
 #Press on button "״להרשמה:
-#search_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "text-btn")))
-#ActionChains(driver).move_to_element(search_element).perform()
-#click_item = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "text-btn")))
-#click_item.click()
+search_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "text-btn")))
+ActionChains(driver).move_to_element(search_element).perform()
+click_item = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "text-btn")))
+click_item.click()
 #Enter first name:
-#driver.find_element_by_id("ember1037").send_keys("ישי")
+driver.find_element_by_id("ember1037").send_keys("ישי")
 #Enter e-mail address:
-#driver.find_element_by_id("ember1039").send_keys("yishaihl@gmail.com")
+driver.find_element_by_id("ember1039").send_keys("yishaihl@gmail.com")
 #Enter password:
-#driver.find_element_by_id("valPass").send_keys("Yisak888%")
+driver.find_element_by_id("valPass").send_keys("Yisak888%")
 #Enter password again:
-#driver.find_element_by_id("ember1043").send_keys("Yisak888%")
+driver.find_element_by_id("ember1043").send_keys("Yisak888%")
 #Press radio button "אני מסכים":
-#driver.execute_script("arguments[0].click();",driver.find_element_by_id("ember1044-id"))
+driver.execute_script("arguments[0].click();",driver.find_element_by_id("ember1044-id"))
 #Press button "...הרשמה":
-#login = driver.find_elements_by_xpath('//*[@id="ember1035"]/button')
-#login[0].click()
+login = driver.find_elements_by_xpath('//*[@id="ember1035"]/button')
+login[0].click()
+
 #Pick a price point:
 time.sleep(4)
 search_element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ember664_chosen"]/a/span')))
